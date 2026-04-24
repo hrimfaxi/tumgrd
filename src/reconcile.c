2bb0a184fc64071caca6e1181280c566dd6eae36
hrimfaxi <outmatch@gmail.com>
2026-04-24 12:07:40 +0800

    refactor with try2
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- src/reconcile.c
+++ src/reconcile.c
@@ -4,6 +4,7 @@
 #include "ipdetect.h"
 #include "log.h"
 #include "runner.h"
+#include "try.h"
 #include "tumgrd.h"
 
 #include <stdio.h>
@@ -34,7 +35,7 @@ static int mark_runtime(struct tumgrd_db *db, const struct tumgrd_node *node, co
 int tumgrd_reconcile_one(struct tumgrd_db *db, struct tumgrd_node *node, bool force) {
   char    detected_ip[64] = {0};
   int64_t now;
-  int     ret;
+  int     err;
   int     ip_changed;
   int     need_apply;
   int     was_error;
@@ -48,18 +49,10 @@ int tumgrd_reconcile_one(struct tumgrd_db *db, struct tumgrd_node *node, bool fo
   log_info("[reconcile] start uid=%s server=%s:%d client_port=%d force=%d old_ip=%s", node->uid, node->server_host,
            node->server_port, node->client_port, force ? 1 : 0, node->current_ip);
 
-  ret = mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_SYNCING, now);
-  if (ret != 0) {
-    log_error("[reconcile] failed to mark syncing uid=%s", node->uid);
-    return -1;
-  }
-
-  ret = detect_public_ip(pick_ip_check_url(node), node->ip_version, detected_ip, sizeof(detected_ip));
-  if (ret != 0) {
-    log_error("[reconcile] detect ip failed uid=%s", node->uid);
-    mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_ERROR, now_unix());
-    return -1;
-  }
+  try2(mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_SYNCING, now), "[reconcile] failed to mark syncing uid=%s",
+       node->uid);
+  try2(detect_public_ip(pick_ip_check_url(node), node->ip_version, detected_ip, sizeof(detected_ip)),
+       "[reconcile] detect ip failed uid=%s", node->uid);
 
   ip_changed = !streqcase(node->current_ip, detected_ip);
   was_error  = streq(node->status, TUMGRD_STATUS_ERROR);
@@ -69,32 +62,23 @@ int tumgrd_reconcile_one(struct tumgrd_db *db, struct tumgrd_node *node, bool fo
            node->current_ip, ip_changed ? 1 : 0, was_error ? 1 : 0, need_apply ? 1 : 0);
 
   if (need_apply) {
-    ret = tumgrd_runner_server_add(node, detected_ip);
-    if (ret != 0) {
-      log_error("[reconcile] server-add failed uid=%s ip=%s", node->uid, detected_ip);
-      mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_ERROR, now_unix());
-      return -1;
-    }
+    try2(tumgrd_runner_server_add(node, detected_ip), "[reconcile] server-add failed uid=%s ip=%s", node->uid, detected_ip);
 
-    ret = tumgrd_runner_reset_local_client(node);
-    if (ret != 0) {
-      log_error("[reconcile] reset local client failed uid=%s", node->uid);
-      mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_ERROR, now_unix());
-      return -1;
-    }
+    try2(tumgrd_runner_reset_local_client(node), "[reconcile] reset local client failed uid=%s", node->uid);
   } else {
     log_info("[reconcile] skip apply uid=%s ip unchanged and force=0", node->uid);
   }
 
-  ret = mark_runtime(db, node, detected_ip, TUMGRD_STATUS_ACTIVE, now_unix());
-  if (ret != 0) {
-    log_error("[reconcile] update runtime failed uid=%s ip=%s", node->uid, detected_ip);
-    return -1;
-  }
-
+  try2(mark_runtime(db, node, detected_ip, TUMGRD_STATUS_ACTIVE, now_unix()), "[reconcile] update runtime failed uid=%s ip=%s",
+       node->uid, detected_ip);
   log_info("[reconcile] success uid=%s ip=%s applied=%d", node->uid, detected_ip, need_apply ? 1 : 0);
 
-  return 0;
+  err = 0;
+err_cleanup:
+  if (err) {
+    mark_runtime(db, node, node->current_ip, TUMGRD_STATUS_ERROR, now_unix());
+  }
+  return err;
 }
 
 int tumgrd_reconcile_all(struct tumgrd_db *db, bool force) {
@@ -108,29 +92,27 @@ int tumgrd_reconcile_all(struct tumgrd_db *db, bool force) {
     return -1;
   }
 
-  err = tumgrd_db_list_nodes(db, &nodes, &count);
-  if (err != 0) {
-    log_error("[reconcile] list nodes failed");
-    return -1;
-  }
-
+  try2(tumgrd_db_list_nodes(db, &nodes, &count), "[reconcile] list nodes failed");
   log_info("[reconcile] reconcile_all count=%zu force=%d", count, force ? 1 : 0);
 
   for (i = 0; i < count; i++) {
-    if (tumgrd_reconcile_one(db, &nodes[i], force) != 0) {
+    if (tumgrd_reconcile_one(db, &nodes[i], force)) {
       failed++;
     }
   }
 
-  tumgrd_db_free_nodes(nodes);
-
   if (failed != 0) {
     log_error("[reconcile] reconcile_all done with failures=%d", failed);
-    return -1;
+    err = -1;
+    goto err_cleanup;
   }
 
   log_info("[reconcile] reconcile_all done");
-  return 0;
+  err = 0;
+
+err_cleanup:
+  tumgrd_db_free_nodes(nodes);
+  return err;
 }
 
 // vim: set sw=2 ts=2 et:
